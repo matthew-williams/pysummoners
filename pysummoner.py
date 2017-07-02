b35217cbc62d19db406f6d90201163daf602d053
Unknown <matthew.williams@programmer.net>
2017-07-01 19:37:40 -0600

    Edge Detection Added

--- pysummoner.py
+++ pysummoner.py
@@ -5,15 +5,26 @@ import time
 
 last_time = time.time()
 
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200,threshold2=300)
+    return processed_img
+
+
+
+
+
+
 while(True):
-    printscreen_pil = ImageGrab.grab(bbox=(10,20,640,400))
-    #printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+    screen = np.array(ImageGrab.grab(bbox=(10,20,640,400)))
+    processed_screen = process_img(screen)
 # Debugging Loop
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
 
 # Show Screen using cv2
-    cv2.imshow('Python Plays Summoners War',np.array(printscreen_pil))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    cv2.imshow('Processed Image',processed_screen)
+    #cv2.imshow('Python Plays Summoners War', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):  # Press Q key to exit
         cv2.destroyAllWindows()
         break
